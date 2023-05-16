d07d98dff9bb31ed54a76d4e7faf62066688e740
Nicolas Lucio <nilucio65@gmail.com>
2023-05-16 09:48:47 -0300

    Adiciona CRUD

--- src/app/client.service.ts
+++ src/app/client.service.ts
@@ -21,6 +21,18 @@ export class ClientService {
     return this.http.post<Client>(this.url, newClient);
   }
 
+  remove(client:Client): Observable<void>{
+   // return this.http.delete<void>(`${this.url}/${client.id}`);
+    return this.http.delete<void>(this.url + "/" + client.id);
+
+  }
+
+  update(client:Client): Observable<Client>{
+    // return this.http.delete<void>(`${this.url}/${client.id}`);
+     return this.http.put<Client>(`${this.url}/${client.id}`, client);
+
+   }
+
 }
 
 
--- src/app/clients/clients.component.ts
+++ src/app/clients/clients.component.ts
@@ -1,6 +1,6 @@
+import { Client } from './../client';
 import { Component, OnInit } from '@angular/core';
 import { ClientService } from '../client.service';
-import { Client } from '../client';
 import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
@@ -12,6 +12,7 @@ export class ClientsComponent implements OnInit{
 
   clients: Client[] = [];
   formGroupClient!: FormGroup;
+  isEditing: boolean = false;
 
   constructor (private clientService: ClientService, private formBuilder: FormBuilder){
     this.formGroupClient = formBuilder.group({
@@ -35,7 +36,16 @@ export class ClientsComponent implements OnInit{
   }
 
   Save(){
-    console.log(this.formGroupClient.value);
+    if(this.isEditing){
+      this.isEditing = false;
+      this.clientService.update(this.formGroupClient.value).subscribe({
+        next: () => {
+          this.loadClients();
+          this.formGroupClient.reset();
+        }
+      })
+    }
+    else{
     this.clientService.save(this.formGroupClient.value).subscribe(
       {
         next: data =>{
@@ -43,7 +53,21 @@ export class ClientsComponent implements OnInit{
         }
       }
     )
+    }
+
     this.loadClients();
+  }
+
+  Exluir(clients: Client): void{
+    this.clientService.remove(clients).subscribe({
+      next:() => this.loadClients()
+
+    });
+  }
+
+  Alterar(clients: Client){
+    this.formGroupClient.setValue(clients)
+    this.isEditing = true;
   }
 
 }
